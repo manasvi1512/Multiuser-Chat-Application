15488a59f8dd8e92ac210bd4a22054c3b7351065
manasvi1512 <158559560+manasvi1512@users.noreply.github.com>
2024-04-26 22:15:11 +0530

    Create Dashboard

---
+++ Dashboard.java
@@ -0,0 +1,68 @@
+package com.dit.chatapp.views;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.awt.event.ActionEvent;
+
+public class DashBoard extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Create the frame.
+	 */
+	public DashBoard(String message) {
+		setResizable(false);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 784, 600);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu chatMenu = new JMenu("Chat");
+		menuBar.add(chatMenu);
+		
+		JMenuItem startChat = new JMenuItem("Start Chat");
+		startChat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					new ClientChatScreen();
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		chatMenu.add(startChat);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(153, 102, 51));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBackground(Color.WHITE);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(DashBoard.class.getResource("/Images/ChatApp Background.png")));
+		contentPane.add(lblNewLabel);
+		setTitle(message);
+		setLocationRelativeTo(null);
+	}
+}
